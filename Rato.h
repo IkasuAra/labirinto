bf8212ee33ee1a6719337f760214ffe1a4c9dd5e
Lucas Tanaka <71355554+IkasuAra@users.noreply.github.com>
2020-10-31 20:10:46 -0300

    Estrutura inicial

---
+++ Rato.h
@@ -0,0 +1,16 @@
+#ifndef RATO_H
+#define RATO_H
+
+struct cenario{
+    int matriz[5][5] = {
+        
+    };
+};
+
+struct Personagem {
+    int movX;
+    int movY;
+    char forma;
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Rato.h"
+
+using namespace std;
+
+int main(){
+    Personagem rato;
+    rato.forma = 'R';
+    
+    return 0;
+}
