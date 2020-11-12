9b74e05c798ca06b9c8eda9c39cdf78888eb29d1
BatataCruaOK <61743678+BatataCruaOK@users.noreply.github.com>
2020-11-12 14:34:16 -0300

    Entrada aleatoria e cenario completo

--- Cenario.h
+++ Cenario.h
@@ -6,8 +6,8 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define ENTRADA 'E'
 #define QUEIJO 'Q'
+#define ENTRADA 'E'
 #define PAREDE '#'
 #define VAZIO '.'
 #define LINHA 5
@@ -17,28 +17,56 @@ using namespace std;
 
 struct Cenario{
     char fundoLabirinto[LINHA][COLUNA];
+    bool fazDenovo = false;
 };
+int randomX(){
+    return rand() % 5;  
+}
 
+int randomY(){
+    return rand() % 5;
+}
 void mostrarCenario(Cenario &fundo){
     Cenario novoCenario;
-    
-    //ALGUEM PODE RANDOMIZAR A ENTRADA PQ EU QUASE SOQUEI MEU PC KSKKKSK
 
+    novoCenario.fundoLabirinto[3][3] = PAREDE;
     novoCenario.fundoLabirinto[3][2] = PAREDE;
     novoCenario.fundoLabirinto[3][1] = PAREDE;
     novoCenario.fundoLabirinto[3][0] = PAREDE;
-    novoCenario.fundoLabirinto[3][3] = PAREDE;
+    novoCenario.fundoLabirinto[4][0] = QUEIJO;
+
+    do{
+        int x = randomX();
+        int y = randomY();
+
+        switch (novoCenario.fundoLabirinto[x][y]){
+            case PAREDE:
+                novoCenario.fazDenovo = true;
+                break;
+
+            case QUEIJO:
+                novoCenario.fazDenovo = true;
+                break;
+            
+            default:
+                novoCenario.fundoLabirinto[x][y] = ENTRADA;
+                novoCenario.fazDenovo = false;
+                break;
+        }
+
+    }while(novoCenario.fazDenovo);
 
 
     for (int i = 0; i < LINHA; i++){
         for(int j = 0; j < COLUNA; j++){
-            if(novoCenario.fundoLabirinto[i][j] != PAREDE and fundo.fundoLabirinto[i][j] != ENTRADA){
+            if(novoCenario.fundoLabirinto[i][j] != PAREDE and novoCenario.fundoLabirinto[i][j] != ENTRADA and novoCenario.fundoLabirinto[i][j] != QUEIJO){
                 novoCenario.fundoLabirinto[i][j] = VAZIO;
-            };
+            }
             cout << novoCenario.fundoLabirinto[i][j] << "\t";
         }
         cout << endl;
     }
+    fflush(stdout);
 }
 
 
